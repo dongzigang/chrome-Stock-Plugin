de3f7cfd4eaee3c1dd390bf92328b2e9876b2ea4
dongzigang <dongzigang@bailuntec.com>
2021-10-28 11:45:56 +0800

    update

--- src/api/api.js
+++ src/api/api.js
@@ -2,16 +2,16 @@ import {request, STOCK_TYPE, calcFixedPirceNumber, formatNumber} from '../assets
 export async function getStockList (data) {
   const params = data.join(',')
   const resp = await request({
-    url: 'https://hq.sinajs.cn/list=' + params,
+    url: 'https://hq.sinajs.cn/list=' + params
   })
   const splitData = resp.split(';\n');
-  let sz = null;
+  const sz = null;
   let aStockCount = 0;
   let usStockCount = 0;
   let hkStockCount = 0;
   let noDataStockCount = 0;
-  let _data = splitData.map((item,index)=>{
-    if(item){
+  const _data = splitData.map((item, index) => {
+    if (item) {
       const code = item.split('="')[0].split('var hq_str_')[1];
       const params = item.split('="')[1].split(',');
       let type = code.substr(0, 2) || 'sh';
@@ -20,15 +20,15 @@ export async function getStockList (data) {
       let fixedNumber = 2;
       if (params.length > 1) {
         if (/^(sh|sz)/.test(code)) {
-          let open = params[1];
-          let yestclose = params[2];
-          let price = params[3];
-          let high = params[4];
-          let low = params[5];
+          const open = params[1];
+          const yestclose = params[2];
+          const price = params[3];
+          const high = params[4];
+          const low = params[5];
           fixedNumber = calcFixedPirceNumber(open, yestclose, price, high, low);
           stockItem = {
             code,
-            area:'A',
+            area: 'A',
             name: params[0],
             open: formatNumber(open, fixedNumber, false),
             yestclose: formatNumber(yestclose, fixedNumber, false),
@@ -37,19 +37,19 @@ export async function getStockList (data) {
             high: formatNumber(high, fixedNumber, false),
             volume: formatNumber(params[8], 2),
             amount: formatNumber(params[9], 2),
-            percent: '',
+            percent: ''
           };
           aStockCount += 1;
         } else if (/^hk/.test(code)) {
-          let open = params[2];
-          let yestclose = params[3];
-          let price = params[6];
-          let high = params[4];
-          let low = params[5];
+          const open = params[2];
+          const yestclose = params[3];
+          const price = params[6];
+          const high = params[4];
+          const low = params[5];
           fixedNumber = calcFixedPirceNumber(open, yestclose, price, high, low);
           stockItem = {
             code,
-            area:'HK',
+            area: 'HK',
             name: params[1],
             open: formatNumber(open, fixedNumber, false),
             yestclose: formatNumber(yestclose, fixedNumber, false),
@@ -58,20 +58,20 @@ export async function getStockList (data) {
             high: formatNumber(high, fixedNumber, false),
             volume: formatNumber(params[12], 2),
             amount: formatNumber(params[11], 2),
-            percent: '',
+            percent: ''
           };
           hkStockCount += 1;
         } else if (/^gb_/.test(code)) {
           symbol = code.substr(3);
-          let open = params[5];
-          let yestclose = params[26];
-          let price = params[1];
-          let high = params[6];
-          let low = params[7];
+          const open = params[5];
+          const yestclose = params[26];
+          const price = params[1];
+          const high = params[6];
+          const low = params[7];
           fixedNumber = calcFixedPirceNumber(open, yestclose, price, high, low);
           stockItem = {
             code,
-            area:'GB',
+            area: 'GB',
             name: params[0],
             open: formatNumber(open, fixedNumber, false),
             yestclose: formatNumber(yestclose, fixedNumber, false),
@@ -80,21 +80,21 @@ export async function getStockList (data) {
             high: formatNumber(high, fixedNumber, false),
             volume: formatNumber(params[10], 2),
             amount: '接口无数据',
-            percent: '',
+            percent: ''
           };
           type = code.substr(0, 3);
           noDataStockCount += 1;
         } else if (/^usr_/.test(code)) {
           symbol = code.substr(4);
-          let open = params[5];
-          let yestclose = params[26];
-          let price = params[1];
-          let high = params[6];
-          let low = params[7];
+          const open = params[5];
+          const yestclose = params[26];
+          const price = params[1];
+          const high = params[6];
+          const low = params[7];
           fixedNumber = calcFixedPirceNumber(open, yestclose, price, high, low);
           stockItem = {
             code,
-            area:'US',
+            area: 'US',
             name: params[0],
             open: formatNumber(open, fixedNumber, false),
             yestclose: formatNumber(yestclose, fixedNumber, false),
@@ -103,40 +103,35 @@ export async function getStockList (data) {
             high: formatNumber(high, fixedNumber, false),
             volume: formatNumber(params[10], 2),
             amount: '接口无数据',
-            percent: '',
+            percent: ''
           };
           type = code.substr(0, 4);
           usStockCount += 1;
         }
-
       } else {
         console.log(`接口不支持该股票 ${code}`)
       }
       return stockItem
     }
   })
-  let result = []
-  _data.forEach((item)=>{
-    if(item){
+  const result = []
+  _data.forEach((item) => {
+    if (item) {
       result.push(item)
     }
   })
   return result
 }
-
-
-
-
-export async function getStockSuggestList(searchText = '', type = '2'){
+export async function getStockSuggestList(searchText = '', type = '2') {
   if (!searchText) {
     alert('请输入关键词查询，如：0000001 或 上证指数')
   }
   const url = `https://suggest3.sinajs.cn/suggest/type=${type}&key=${encodeURIComponent(searchText)}`;
   try {
-    const response = await request({url:url});
+    const response = await request({url: url});
     const text = response.slice(18, -1);
     const tempArr = text.split(';');
-    let result =  [];
+    const result = [];
     tempArr.forEach((item) => {
       const arr = item.split(',');
       let code = arr[0];
@@ -153,7 +148,7 @@ export async function getStockSuggestList(searchText = '', type = '2'){
       if (STOCK_TYPE.includes(code.substr(0, 2)) && !code.startsWith('us.')) {
         result.push({
           label: `${code} | ${arr[4]}`,
-          description: arr[7] && arr[7].replace(/"/g, ''),
+          description: arr[7] && arr[7].replace(/"/g, '')
         });
       }
     });
